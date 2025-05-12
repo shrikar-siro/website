05d18ceb321c0af50da0b2c93b1e95379d2b5a12
unknown <siro.shrikar@gmail.com>
2025-05-12 15:49:04 +0400

    started navbar work

---
+++ portfolio/components/ui/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return(
+        <header className = "bg-black">
+            <img className = "w-16" src = "public/images/profile.jpeg" alt = "..."></img>
+        </header>
+    );
+}
