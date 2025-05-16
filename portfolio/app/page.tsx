1f392ba609901cbdd838363674a0df5e9ffceb80
unknown <siro.shrikar@gmail.com>
2025-05-16 12:34:06 +0400

    user interface changes

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -28,11 +28,24 @@ import { setupIntersectionAnimations } from "../app/animateIfInView"
 
 
 
+
 export default function Home() {
   // made the script runnable, so now we can use useEffect to call the setup function.
   useEffect(() => {
     setupIntersectionAnimations();
   }, []);
+
+  const mySkills = [ 
+    {name: "Python", icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg'},
+    {name: "Java", icon:'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-plain.svg'},
+    {name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"},
+    {name: "Amazon Web Services (AWS)", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg"},
+    {name: "React.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"},
+    {name: "C", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"},
+    {name: "PyTorch", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg"},
+    {name: "Tensorflow", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg"}
+
+  ];
 
   return (
     // add the navbar component here - or add it in the layout page.
@@ -45,7 +58,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="about" className = "py-35 place-content-center scroll-mt-20">
+      <section id="about" className = "py-35 place-content-center -scroll-mt-20">
         {/** have a big div as the container div, and 2 children divs. */}
         <div className = "lg:flex lg:gap-20 items-start p-15 md:p-10 sm:p-5 mx-auto">
           <div className = "flex flex-shrink-0 justify-center">
@@ -65,52 +78,58 @@ export default function Home() {
           Experience
         </div>
         <div className = "flex flex-col experiences animate justify-center mt-10 p-15 md:p-10 sm:p-5 mx-auto">
-          <Card className = " w-full mx-auto mb-5 bg-transparent border border-gray-300 transform transition hover:-translate-y-1 duration-300">
+          <Card className = " w-full mx-auto mb-5 bg-transparent border-2 transform transition duration-300 hover:border-blue-500">
             <CardHeader>
               <CardTitle>
                 <div className = "flex flex-row gap-2 items-center">
                   <img className = "w-10 h-10" src = "stLogo.jpg" alt = "..."></img>
-                  <p className = "text-lg">Incoming Software Engineering Intern</p>
+                  <div className = "text-lg">
+                    Incoming Software Engineering Intern
+                    <CardDescription>ST Engineering</CardDescription>
+                  </div>
                 </div>
               </CardTitle>
-              <CardDescription>ST Engineering</CardDescription>
             </CardHeader>
             <CardContent>
-                <ul className = "font-normal text-lg">
+                <ul className = "font-normal text-md">
                   <li>• Summer Software Engineering Intern on the DevOps team.</li>
                 </ul>
             </CardContent>
           </Card>
-          <Card className = " w-full mx-auto mb-5 bg-transparent border border-gray-300 transform transition hover:-translate-y-1 duration-300">
+          <Card className = " w-full mx-auto mb-5 bg-transparent border-2 transform transition duration-300 hover:border-blue-500">
             <CardHeader>
               <CardTitle>
                 <div className = "flex flex-row gap-2 items-center">
                   <img className = "w-10 h-10" src = "uvaIcon.png" alt = "..."></img>
-                  <p className = "text-lg">Undergraduate Research Intern</p>
+                  <div className = "text-lg">
+                    Undergraduate Research Intern
+                    <CardDescription>UVA School of Data Science</CardDescription>
+                  </div>
                 </div>
               </CardTitle>
-              <CardDescription>UVA School of Data Science</CardDescription>
             </CardHeader>
             <CardContent>
-                <ul className = "font-normal text-lg">
+                <ul className = "font-normal text-md">
                   <li>• Develop a visual analytics platform using Python and SQL to analyze multi-modal neuroscience datasets.</li>
                   <li>• Support early diagnosis and treatment strategies for cognitive disorders through deep learning architectures.</li>
                   <li>• Optimize model accuracy and provide an intuitive user interface for data exploration.</li>
                 </ul>
             </CardContent>
           </Card>
-          <Card className = "w-full mx-auto mb-5 bg-transparent border border-gray-300 transform transition hover:-translate-y-1 duration-300">
+          <Card className = "w-full mx-auto mb-5 bg-transparent border-2 transform transition duration-300 hover:border-blue-500">
             <CardHeader>
               <CardTitle>
                 <div className = "flex flex-row gap-2 items-center">
                   <img className = "w-10 h-10" src = "uvaEngrLogo.png" alt = "..."></img>
-                  <p className = "text-lg">Undergraduate Teaching Assistant</p>
+                  <div className = "text-lg">
+                    Undergraduate Teaching Assistant
+                    <CardDescription>UVA School of Engineering</CardDescription>
+                  </div>
                 </div>
               </CardTitle>
-              <CardDescription>UVA School of Engineering</CardDescription>
             </CardHeader>
             <CardContent>
-                <ul className = "font-normal text-lg">
+                <ul className = "font-normal text-base">
                   <li>• Teaching Assistant for CS 2130 - Computer Systems & Organization I</li>
                 </ul>
             </CardContent>
@@ -132,25 +151,26 @@ export default function Home() {
                   align: "start",
                 }}
                 className="w-full"
+                orientation="horizontal"
               >
               <CarouselContent>
                 <CarouselItem className = "md:basis-1/2 lg:basis-1/1">
-                  <div className = "lg:h-[500px] md:h-[500px] border-2 border-gray-200 border-opacity-60 rounded-md overflow-hidden transform transition duration-150 hover:bg-gray-900 flex flex-col">
-                    <img src = "laLigaInsights.png" className = "w-full h-[250px] object-cover object-center"></img>
+                  <div className = "lg:h-[500px] md:h-[500px] border-2 border-opacity-60 rounded-md overflow-hidden transform transition duration-150 hover:bg-black/30">
+                    <img src = "laLigaInsights.png" className = "w-full h-[300px] object-cover object-center"></img>
                     <div className = "lg:p-6 md:p-4 sm:p-4 hidden md:block flex-grow text-center">
                       <h1 className = "text-lg font-semibold mb-3 text-center">LaLiga Insights</h1>
-                      <p className = "lg:text-md md:text-base mx-auto leading-relaxed mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ipsum similique, nisi, maiores ipsam 
+                      <p className = "lg:text-base md:text-sm mx-auto leading-relaxed mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ipsum similique, nisi, maiores ipsam 
                         natus reiciendis 
                         consequuntur alias est atque.</p>
                     </div>
                   </div>
                 </CarouselItem>
                 <CarouselItem className = "md:basis-1/2 lg:basis-1/1">
-                  <div className = "lg:h-[500px] md:h-[500px] border-2 border-gray-200 border-opacity-60 rounded-md overflow-hidden transform transition duration-150 hover:bg-gray-900">
-                    <img src = "LungCancerClassifier.png" className = "w-full h-[250px] object-cover object-center"></img>
+                  <div className = "lg:h-[500px] md:h-[500px] border-2 border-opacity-60 rounded-md overflow-hidden transform transition duration-150 hover:bg-black/30">
+                    <img src = "LungCancerClassifier.png" className = "w-full h-[300px] object-cover object-center"></img>
                     <div className = "lg:p-6 md:p-4 sm:p-4 hidden md:block flex-grow text-center">
                       <h1 className = "text-lg font-semibold mb-3 text-center">Lung Cancer Classifier</h1>
-                      <p className = "lg:text-md md:text-base mx-auto leading-relaxed mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ipsum similique, nisi, maiores ipsam 
+                      <p className = "lg:text-base md:text-sm mx-auto leading-relaxed mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam ipsum similique, nisi, maiores ipsam 
                         natus reiciendis 
                         consequuntur alias est atque iste sequi beatae fugiat 
                         reprehenderit quis quo repudiandae neque earum.</p>
@@ -165,6 +185,20 @@ export default function Home() {
         </div>
       </section>
 
+      <section id="skills" className="py-40 place-content-center -scroll-mt-10">
+        <div className = "text-4xl text-center font-bold">
+          Skills
+        </div>
+        {/*grid of cards with each skill pulled from list above.*/}
+        <div className = "m-auto mt-5 p-15 md:p-10 sm:p-5 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-4 experiences animate">
+          {mySkills.map((item, index) =>(
+            <div key = {index} className = "justify-center items-center p-10 border-1 rounded-md transform transition duration-200 hover:border-blue-500">
+                <img className = "w-auto h-20 lg:h-30 md:h-25 mx-auto mb-3" src={item.icon} />
+                <h3 className = "text-xl text-center">{item.name}</h3>
+            </div>
+          ))}
+        </div>
+      </section>
 
     </>
   );
